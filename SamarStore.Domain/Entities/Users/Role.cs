a371b90c48c66b93dc75300e5797ff43ea82e566
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2023-09-28 13:09:07 +0330

    +User Enitity +Role Entity +UserInRole Entity

---
+++ SamarStore.Domain/Entities/Users/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace SamarStore.Domain.Entities.Users
+{
+    public class Role
+    {
+        public long Id { get; set; }    
+        public string Name { get; set; }    
+        public ICollection<UserInRole> UserInRoles { get; set; }
+    }
+}
---
+++ SamarStore.Domain/Entities/Users/UserInRole.cs
@@ -0,0 +1,11 @@
+﻿namespace SamarStore.Domain.Entities.Users
+{
+    public class UserInRole
+    {
+        public long Id { get; set; }    
+        public virtual User User { get; set; }  
+        public long UserId { get; set; }    
+        public virtual Role Role { get; set; }  
+        public long RoleId { get; set; }    
+    }
+}
