9d43ba0cc668d53287218dfe94a2cdc61c830bf4
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2023-09-28 13:18:34 +0330

    +DataBaseContext Class +SetUp DbContext (DataBase) Options

---
+++ SamarStore.Persistence/Context/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamarStore.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamarStore.Persistence.Context
+{
+    internal class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions opetions) : base (opetions)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }  
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UsersInRoles { get; set; } 
+
+    }
+}
